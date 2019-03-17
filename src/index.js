43e836aefd64c56a4df6593678d2b7686f71a209
Julanick <julanick@gmail.com>
2019-03-17 21:02:21 +0300

    docs: Imlement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2 != 0) return false;
+
+  var openedBrackets = [];
+  var closedBrackets = [];
+
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    openedBrackets.push(bracketsConfig[i][0]);
+    closedBrackets.push(bracketsConfig[i][1]);
+  }
+
+  var openedStack = [];
+
+  for (var i = 0; i < str.length; i++) {
+    if (openedStack.length == 0) {
+      if (!openedBrackets.includes(str[i])) return false;
+      openedStack.push(str[i]);
+    } else {
+      var lastOpenedIndex = openedBrackets.indexOf(openedStack[openedStack.length - 1]);
+      if (str[i] == closedBrackets[lastOpenedIndex]) {
+        openedStack.pop();
+      } else if (openedBrackets.includes(str[i])) {
+        openedStack.push(str[i]);
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return openedStack.length == 0;
 }
